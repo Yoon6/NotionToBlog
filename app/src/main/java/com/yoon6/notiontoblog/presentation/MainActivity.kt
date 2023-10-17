1961efa6ee155fbe9454594c7dfb3279e88c7398
daeyoon.choi <daeyoon.choi@phoenixdarts.com>
2023-10-17 11:41:10 +0900

    configure bottom navigation

---
+++ app/src/main/java/com/yoon6/notiontoblog/presentation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.yoon6.notiontoblog.presentation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.yoon6.notiontoblog.R
+import com.yoon6.notiontoblog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.let {
+            binding.bottomNavigation.setupWithNavController(it.findNavController())
+        }
+    }
+}
---
+++ app/src/main/java/com/yoon6/notiontoblog/presentation/notion/NotionFragment.kt
@@ -0,0 +1,45 @@
+package com.yoon6.notiontoblog.presentation.notion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.yoon6.notiontoblog.R
+import com.yoon6.notiontoblog.databinding.FragmentNotionBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class NotionFragment : Fragment() {
+
+    private var _binding: FragmentNotionBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentNotionBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
